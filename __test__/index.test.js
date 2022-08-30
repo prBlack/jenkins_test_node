3873ae8300e96bdeebf9dee64ef9b05f3b96f784
Vlad Mastenitsa <vlad.v.master@gmail.com>
2022-08-30 12:28:58 +0300

    initital commit

---
+++ __test__/index.test.js
@@ -0,0 +1,19 @@
+const request = require("supertest");
+const app = require("../index");
+
+describe("Get route", () => {
+  it("page should return hello world", async () => {
+    try {
+	const res = await request(app).get("/");
+    	expect(res.statusCode).toEqual(200);
+    	expect(res.body).toEqual("Hello world");
+    } catch(err) {
+	;
+    }
+    
+  });
+});
+
+afterAll(async () => {
+  await app.close();
+});
